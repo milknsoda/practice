1cb3a42402ea914d15b8e3bf3ae3894118d1d65f
milknsoda <milknsoda9327@gmail.com>
2019-07-25 21:35:16 +0900

    190725 | 파리퇴치 문제 풀이 완료(재확인 필요)

--- SWEA/파리퇴치.py
+++ SWEA/파리퇴치.py
@@ -1,6 +1,6 @@
 testcase = int(input())
 
-for case in range(testcase)
+for case in range(testcase):
     # N x N 의 배열을 만든다.
     n, m = map(int, input().split())
     arrangement = []
@@ -8,17 +8,15 @@ for case in range(testcase)
     for i in range(n):
         line = list(map(int, input().split()))
         arrangement.append(line)
-    # M x M 의 파리채안의 파리 수의 합 중에서 가장 큰 수를 출력한다.
+    # M x M 의 파리채 범위 안의 파리 수의 합 중에서 가장 큰 수를 출력한다.
     result = 0
     for row in range(n):
         for column in range(n):
             total = 0
-            for swatter in range(m):
-                if row + (m-1) < n and column + (m-1) < n:
-                    total += arrangement[row+swatter][column+swatter]
-                    if swatter != 0:
-                        total += arrangement[row][column + swatter]
-                        total += arrangement[row+swatter][column]
+            if row + (m-1) < n and column + (m-1) < n:
+                for swat_r in range(m):
+                    for swat_c in range(m):
+                        total += arrangement[row+swat_r][column+swat_c]
             if result < total:
                 result = total
     print(f'#{case+1} {result}')
