248f6c62d77b5fb7addc84c5d8fa88e45e20c7fa
milknsoda <milknsoda9327@gmail.com>
2019-07-30 18:04:00 +0900

    190730 | python

--- SWEA/test.py
+++ SWEA/test.py
@@ -1,16 +1,14 @@
-def my_sqrt(n):
-    for i in range(1, n):
-        if i**2 <= n < (i+1)**2:
-            l = i
-            r = i + 1
-    if n <= 0:
-        return '잘못된 입력'
-    elif n == 1:
-        return 1
-    while abs(l**2 - n) >= 1e-10:
-        if n < ((r + l) / 2)**2:
-            r = (r + l) / 2
-        elif n > ((r + l) / 2)**2:
-            l = (r + l) / 2
-        print(l, r)
-    print(l)
+a = [0, 1, 1, 1, 0, 1]
+k = 1
+
+count = 0
+crossword = 0
+
+for i in a:
+    if i == 1:
+        count += 1
+    else:
+        if count == k:
+            crossword += 1
+        count = 0
+print(crossword)
